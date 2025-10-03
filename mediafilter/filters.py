3595c1eea3e5c592960defa99a44562a4cf47630
greentay20 <greenwaldtaylor@gmail.com>
2025-10-02 23:23:56 -0400

    Reduce edge flickering and parameter tuning

--- mediafilter/filters.py
+++ mediafilter/filters.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 as cv
 from sklearn.cluster import MiniBatchKMeans
 from collections import deque
+from kneed import KneeLocator
 
 def normalize_size(img, max_side=1280):
     h, w = img.shape[:2]
@@ -13,7 +14,7 @@ def normalize_size(img, max_side=1280):
     norm_img = cv.resize(img, (norm_w, norm_h), interpolation=cv.INTER_AREA)
     return norm_img
 
-def get_canny_threshs(img):
+def get_canny_threshs(img): # thresholds for edge detection 
     grad_x = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=3)
     grad_y = cv.Sobel(img, cv.CV_64F, 0, 1, ksize=3)
     grad_mag = cv.magnitude(grad_x, grad_y)
@@ -24,7 +25,7 @@ def get_canny_threshs(img):
         upper = min(lower + 20, 255)
     return lower, upper
 
-def get_sigma(img):
+def get_sigma(img): # calculate how much blur to use for gaussianBlur
     sharpness = cv.Laplacian(img, cv.CV_64F).var()
     base_sigma = 1.5
     if sharpness > 500:    
@@ -64,19 +65,30 @@ def get_kmeans(pix_colors, num_clusts):
     return new_kmeans
 
 # reduce edge flickering in video
-edge_buffer = deque(maxlen=10)
-def smooth_edges(curr_edges, blend_weight=0.3):
+edge_buffer = deque(maxlen=3)
+def smooth_edges(curr_edges, min_weight=0.4, max_weight=0.8): # small weight -> more past frame influence
     global edge_buffer
-    edge_buffer.append(curr_edges.astype(np.float32))
-    if len(edge_buffer) == 1:
-        return curr_edges
+    curr_edges = curr_edges.astype(np.float32)
+
+    if len(edge_buffer) == 0:
+        edge_buffer.append(curr_edges)
+        return curr_edges.astype(np.uint8)
     
+    prev_smoothed = edge_buffer[-1] # get last frame edges
+    edge_diff = np.mean(cv.absdiff(curr_edges.astype(np.uint8), prev_smoothed.astype(np.uint8)))
+    motion = edge_diff / 255.0
+
+    blend_weight = min_weight + (max_weight - min_weight) * motion # more motion -> larger weight -> less past frame influence
+
+    edge_buffer.append(curr_edges)
+
     smoothed = blend_weight * edge_buffer[-1]
     remaining_weight = (1 - blend_weight) / (len(edge_buffer) - 1)
     for i in range(len(edge_buffer) - 1):
         smoothed += remaining_weight * edge_buffer[i]
+    smoothed = np.clip(smoothed, 0, 255).astype(np.uint8)
+    return smoothed
     
-    return (smoothed > 127).astype(np.uint8) * 255
 
 kmeans = None
 def get_cartoon_frame(frame, frame_idx, for_video=False):
@@ -84,18 +96,18 @@ def get_cartoon_frame(frame, frame_idx, for_video=False):
     if for_video == False:
         frame = normalize_size(frame, max_side=1280)
 
-    smooth = cv.bilateralFilter(frame, d=6, sigmaColor=100, sigmaSpace=50)
+    smooth = cv.bilateralFilter(frame, d=7, sigmaColor=100, sigmaSpace=75)
     pixel_colors = smooth.reshape((-1, 3)) 
 
     rt_every_frame = 30 # retrain and update color centroids every n frames
     if kmeans == None or frame_idx % rt_every_frame == 0: # if kmeans not created or time for new fitting
         if for_video == True:
             sample = pixel_colors[np.random.choice(len(pixel_colors), size=5000, replace=False)]
-            elbow_k = get_k_elbow(sample, k_min=24, k_max=64, step=4, for_vid=True)
+            elbow_k = get_k_elbow(sample, k_min=12, k_max=32, step=4, for_vid=True)
             kmeans = get_kmeans(pixel_colors, num_clusts=elbow_k) # get new centroids (video)
         else:
             sample = pixel_colors[np.random.choice(len(pixel_colors), size=50000, replace=False)]
-            elbow_k = get_k_elbow(sample, k_min=24, k_max=64, step=4, for_vid=False)
+            elbow_k = get_k_elbow(sample, k_min=12, k_max=32, step=4, for_vid=False)
             kmeans = get_kmeans(pixel_colors, num_clusts=elbow_k) # get new centroids (img)
 
     labels = kmeans.predict(pixel_colors) # pixels to color clusters
@@ -103,16 +115,16 @@ def get_cartoon_frame(frame, frame_idx, for_video=False):
     quantized = quantized.reshape(smooth.shape)
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    
     lower_th, upper_th = get_canny_threshs(gray)
     sigma = get_sigma(gray)
     edges = get_edges(gray, 5, lower_th * 1.5, upper_th * 1.5, sigma)
-    edges = cv.dilate(edges, np.ones((2,2), np.uint8), iterations=1)
 
     if for_video == True:
         edges = smooth_edges(edges)
 
     cartoon_frame = quantized.copy()
-    df = 0.4  # larger value -> lighter edges 
+    df = 0.4  # dark factor (lower value -> darker edges)
     cartoon_frame[edges != 0] = (cartoon_frame[edges != 0] * df).astype(np.uint8)
 
     return cartoon_frame
@@ -129,6 +141,9 @@ def get_k_elbow(pix_colors, k_min, k_max, step, for_vid):
         kmeans = MiniBatchKMeans(n_clusters=k, random_state=0, batch_size=bs)
         kmeans.fit(pix_colors)
         inertias.append(kmeans.inertia_)
-    diffs = np.diff(inertias)
-    elbow_idx = np.argmin(diffs)
-    return ks[elbow_idx]
+
+    kl = KneeLocator(ks, inertias, curve="convex", direction="decreasing")
+    best_k = kl.knee
+    if best_k == None:
+        best_k = ks[len(ks) // 2]
+    return best_k
